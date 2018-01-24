85f03f04a29bcd7a762b58e137c90474382c7cce
Caoanlong <caoanlong@gmail.com>
2018-01-24 11:12:01 +0800

    添加修改

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = require('express')()
 const multer = require('multer')
 const logger = require('morgan')
+const axios = require('axios')
 
 app.use(logger('dev'))
 
@@ -21,6 +22,24 @@ app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Methods', 'POST, OPTIONS')
 	res.header('Access-Control-Allow-Headers', 'Content-Type,Accept,X-Access-Token')
 	next()
+})
+
+app.get('/getOpenImg', (req, res) => {
+	let url = 'http://www.apiopen.top:88/meituApi'
+	// let params = {
+	// 	pn: req.query.pn || 0,
+	// 	rn: req.query.rn || 30,
+	// 	tag1: req.query.tag1,
+	// 	tag2: req.query.tag2,
+	// 	ie: 'utf8',
+	// }
+	let params = {
+		page: req.query.page
+	}
+	axios.get(url, {params: params}).then(response => {
+		console.log(response.data)
+		res.json(response.data)
+	})
 })
 
 /* 上传单个图片 */
