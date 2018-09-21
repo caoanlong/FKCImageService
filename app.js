7ad22bde1af14b420ddd9b1f2c6ad55fe7768502
Caoanlong <caoanlong@gmail.com>
2018-09-21 14:23:56 +0800

    modify let to const

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const app = require('express')()
 const multer = require('multer')
 const logger = require('morgan')
-const axios = require('axios')
 
 app.use(logger('dev'))
 
-let storage = multer.diskStorage({
+const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
 		// cb(null, './public/uploads')
 		cb(null, '/mydatadisk/images')
@@ -14,7 +13,7 @@ let storage = multer.diskStorage({
 		cb(null, file.fieldname + '-' + Date.now() + '.' + file.mimetype.split('/')[1])
 	}
 })
-let upload = multer({ storage: storage })
+const upload = multer({ storage })
 
 //设置跨域
 app.use(function (req, res, next) {
@@ -24,31 +23,10 @@ app.use(function (req, res, next) {
 	next()
 })
 
-app.get('/getOpenImg', (req, res) => {
-	let url = 'http://www.apiopen.top:88/meituApi'
-	// let params = {
-	// 	pn: req.query.pn || 0,
-	// 	rn: req.query.rn || 30,
-	// 	tag1: req.query.tag1,
-	// 	tag2: req.query.tag2,
-	// 	ie: 'utf8',
-	// }
-	let params = {
-		page: req.query.page
-	}
-	axios.get(url, {params: params}).then(response => {
-		console.log(response.data)
-		res.json(response.data)
-	})
-})
-
 /* 上传单个图片 */
 app.post('/uploadImg', upload.single('file'), function (req, res, next) {
 	if (!req.file) {
-		res.json({
-			code: 1,
-			msg: '图片为空'
-		})
+		res.json({ code: 1, msg: '图片为空' })
 		return
 	}
 	res.json({
